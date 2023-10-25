e9fc73116ec1460142a7a3358d804c9f69e323f5
Igorello <orl715320@gmail.com>
2023-10-25 22:04:28 +0300

    generateCells

--- app/src/main/java/com/example/cells/MainActivity.java
+++ app/src/main/java/com/example/cells/MainActivity.java
@@ -1,22 +1,24 @@
 package com.example.cells;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button[][] cells;
+    TextView[][] cells;
     TextView mines;
 
-    final int MINESCONST = 3;
+    final int MINESCONST = 15;
     int minesCurrent = 3;
     final int WIDTH = 10;
     final int HEIGHT = 15;
@@ -31,24 +33,34 @@ public class MainActivity extends AppCompatActivity {
         mines.setText(""+ minesCurrent +" / "+MINESCONST);
 
         generate();
+        generateMines();
+
+    }
+
+    public void generateMines(){
+        for(int i=0; i<MINESCONST; i++){
+            int positionX = getRandomPosition(HEIGHT);
+            int positionY = getRandomPosition(WIDTH);
+            cells[positionX][positionY].setText(String.valueOf(-1));
+        }
+    }
+
+    public int getRandomPosition(int value){
+        return new Random().nextInt(value);
     }
 
     public void generate(){
         GridLayout layout = findViewById(R.id.grid);
-        layout.removeAllViews();
-        layout.setColumnCount(WIDTH);
-        cells = new Button[HEIGHT][WIDTH];
-        LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        generateCells();
 
         for(int i=0; i<HEIGHT;i++){
             for(int j=0; j<WIDTH;j++){
-                cells[i][j] = (Button) inflater.inflate(R.layout.cell, layout, false);
-            }
-        }
-        for(int i=0; i<HEIGHT;i++){
-            for(int j=0; j<WIDTH;j++){
-                cells[i][j].setText(""+j+HEIGHT*i);
-                cells[i][j].setTag(""+j+HEIGHT*i);
+                cells[i][j].setText(String.valueOf(1));
+                if((i+j)%2 == 0) cells[i][j].setBackgroundColor(ContextCompat.getColor(this, R.color.lightGreen));
+                else cells[i][j].setBackgroundColor(ContextCompat.getColor(this, R.color.darkGreen));
+
+
                 cells[i][j].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -60,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
                     public boolean onLongClick(View v) {
                         v.setBackgroundColor(Color.BLUE);
                         minesCurrent--;
-                        mines.setText(""+ minesCurrent +" / "+MINESCONST);
+                        mines.setText(String.valueOf(minesCurrent +" / "+MINESCONST));
                         if(minesCurrent==0) {
                             Toast.makeText(getApplicationContext(), "WIN!!!", Toast.LENGTH_LONG).show();
                         }
@@ -69,6 +81,21 @@ public class MainActivity extends AppCompatActivity {
                 });
 
                 layout.addView(cells[i][j]);
+            }
+        }
+    }
+
+    public void generateCells(){
+        LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        GridLayout layout = findViewById(R.id.grid);
+        layout.removeAllViews(); // Удаляем все существующие представления в GridLayout (если они есть)
+        layout.setColumnCount(WIDTH); // Устанавливаем количество столбцов в GridLayout (WIDTH - константа)
+
+        cells = new TextView[HEIGHT][WIDTH];
+        for(int i=0; i<HEIGHT;i++){ // Создаем цикл для создания кнопок (Cells)
+            for(int j=0; j<WIDTH;j++){
+                // Используем LayoutInflater для надувания макета кнопки из R.layout.cell
+                cells[i][j] = (TextView) inflater.inflate(R.layout.cell, layout, false);
             }
         }
     }
