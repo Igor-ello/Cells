3e95478c46fbfdc4c084bc9ad9b194d6877579b4
Igorello <orl715320@gmail.com>
2023-10-27 02:07:37 +0300

    rebuld: view

--- app/src/main/java/com/example/cells/MainActivity.java
+++ app/src/main/java/com/example/cells/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.cells;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
-import android.graphics.Color;
+import android.annotation.SuppressLint;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
     TextView[][] cells;
     String[][] cellsText;
     boolean[][] opened; // Массив для отслеживания открытых клеток
-    TextView minesCounter, refreshButton;
+    TextView minesCounter, refresh, refreshButton;
+    LinearLayout imStopGame;
 
     final int MINESCONST = 35;
     int minesCurrent = MINESCONST;
@@ -30,16 +32,24 @@ public class MainActivity extends AppCompatActivity {
     final int HEIGHT = 20 ;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imStopGame = findViewById(R.id.imStopGame);
 
         generateCells();
         generate();
 
-        refreshButton = findViewById(R.id.refresh);
+        refresh = findViewById(R.id.refresh);
+        refresh.setOnClickListener(view -> {
+            resetCells();
+            generate();
+        });
+        refreshButton = findViewById(R.id.refreshButton);
         refreshButton.setOnClickListener(view -> {
+            imStopGame.setVisibility(View.GONE);
             resetCells();
             generate();
         });
@@ -80,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
 
         for (int i = 0; i < HEIGHT; i++) {
             for (int j = 0; j < WIDTH; j++) {
+                cells[i][j].setText("");
                 cellsText[i][j] = "0";
             }
         }
@@ -105,11 +116,15 @@ public class MainActivity extends AppCompatActivity {
                 if (!cellsText[i][j].equals(String.valueOf(-1)))
                     cellsText[i][j] = String.valueOf(setTextToCell(i, j));
 
+
                 cells[i][j].setOnClickListener(view -> {
                     if (cellsText[finalI][finalJ].equals("-1")) {
                         if((finalI+finalJ)%2 == 0)
                             view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.darkGray));
                         else view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.lightGray));
+                        setPictureToCell(view, ContextCompat.getDrawable(getApplicationContext(), R.drawable.explosion));
+
+                        imStopGame.setVisibility(View.VISIBLE);
                         Toast.makeText(getApplicationContext(), "LOSE!!!", Toast.LENGTH_LONG).show();
                     } else {
                         openCells(finalI, finalJ);
@@ -118,7 +133,10 @@ public class MainActivity extends AppCompatActivity {
                 cells[i][j].setOnLongClickListener(view -> {
                     if(minesCurrent-1 >= 0){
                         minesCurrent--;
-                        setFlagToCell(view);
+                        if(hasIcon(view)) {
+                            removePictureFromCell(view);
+                        }//TODO убрать флаг, если он есть
+                        else setPictureToCell(view, ContextCompat.getDrawable(getApplicationContext(), R.drawable.flag));
                     }
 
                     minesCounter.setText(String.valueOf(minesCurrent + " / " + MINESCONST));
@@ -176,20 +194,45 @@ public class MainActivity extends AppCompatActivity {
 
         return count;
     }
-    public void setFlagToCell(View view) {
+
+    public void setPictureToCell(View view, Drawable drawable) {
         Drawable[] layers = new Drawable[2];
 
         Drawable currentBackground = view.getBackground();
         layers[0] = currentBackground; // Текущий фон
         // Получаем значок (изображение) из ресурсов
-        Drawable iconDrawable = ContextCompat.getDrawable(getApplicationContext(), R.drawable.flag);
+        Drawable iconDrawable = drawable;
         layers[1] = iconDrawable; // Значок
         LayerDrawable layerDrawable = new LayerDrawable(layers);
         // Устанавливаем LayerDrawable как фон элемента View
         view.setBackground(layerDrawable);
     }
+    public void removePictureFromCell(View view) {
+        Drawable[] layers = new Drawable[1];
 
-    public void setColorToCells() {
+        Drawable currentBackground = view.getBackground();
+        layers[0] = currentBackground; // Текущий фон
+        LayerDrawable layerDrawable = new LayerDrawable(layers);
+
+        view.setBackground(layerDrawable);
+    }
+    public boolean hasIcon(View view) {
+        Drawable background = view.getBackground();
+        if (background instanceof LayerDrawable) {
+            LayerDrawable layerDrawable = (LayerDrawable) background;
+            int numberOfLayers = layerDrawable.getNumberOfLayers();
+            // Проверяем, есть ли второй слой, который будет значком
+            if (numberOfLayers >= 2) {
+                Drawable iconDrawable = layerDrawable.getDrawable(1); // Второй слой
+                return iconDrawable != null;
+            }
+        }
+        return false; // Значок отсутствует
+    }
+
+
+
+    public void setColorToCells() { //TODO покрасить цифры
         for (int i = 0; i < HEIGHT; i++) {
             for (int j = 0; j < WIDTH; j++) {
                 if ((i + j) % 2 == 0) cells[i][j].setBackgroundColor(ContextCompat.getColor(this, R.color.lightGreen));
